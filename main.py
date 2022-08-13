7042aa55d8be9385f613aeab291a1db1703b277f
Ribhav Sabharwal <ribhavsabharwal50@gmail.com>
2022-08-13 18:24:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import mean_var_std.py
+# This entrypoint file to be used in development. Start by reading README.md
+import mean_var_std
+from unittest import main
 
-print(calculate(1,2,3,4,5,6,7,8,9))
+print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
